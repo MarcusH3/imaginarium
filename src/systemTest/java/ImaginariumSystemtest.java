75f4ce94e87742bcd3791c47a9eac89c7d7f0733
marcusH3 <marcus.hurtigh@outlook.com>
2023-10-07 12:55:05 +0200

    checkstyleTestFix

--- src/systemTest/java/ImaginariumSystemtest.java
+++ src/systemTest/java/ImaginariumSystemtest.java
@@ -1,3 +1,8 @@
+import static org.hibernate.validator.internal.util.Contracts.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.springframework.test.util.AssertionErrors.assertNotNull;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -6,12 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import static org.junit.jupiter.api.Assertions.*;
 
+/**.
+    * ImaginariumSystemtest
+    */
 public class ImaginariumSystemtest {
 
   private static RestTemplate restTemplate;
-  private final String BASE_URL = "http://localhost:8080";
+  private final String baseUrl = "http://localhost:8080";
 
   @BeforeAll
   public static void setup() {
@@ -21,7 +28,7 @@ public class ImaginariumSystemtest {
   @Test
   public void testFetchRandomImage() {
     ResponseEntity<String> response =
-        restTemplate.getForEntity(BASE_URL + "/images/random", String.class);
+        restTemplate.getForEntity(baseUrl + "/images/random", String.class);
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertTrue(response.hasBody(), "Image not returned");
   }
@@ -29,12 +36,11 @@ public class ImaginariumSystemtest {
   @Test
   public void testFetchRandomQuote() {
     ResponseEntity<String> response =
-        restTemplate.getForEntity(BASE_URL + "/api/quotes/random", String.class);
+        restTemplate.getForEntity(baseUrl + "/api/quotes/random", String.class);
     assertEquals(HttpStatus.OK, response.getStatusCode());
     assertNotNull(response.getBody(), "Quote not returned");
     assertFalse(response.getBody().isEmpty(), "Quote is empty");
   }
-
 
   @AfterAll
   public static void tearDown() {
