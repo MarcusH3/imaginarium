fad777c4d77665e032f25f30b77c6db4b3ae3f29
marcusH3 <marcus.hurtigh@outlook.com>
2023-10-07 16:02:16 +0200

    Controller fix

--- src/test/java/com/example/imaginarium/ImageControllerTest.java
+++ src/test/java/com/example/imaginarium/ImageControllerTest.java
@@ -29,6 +29,6 @@ class ImageControllerTest {
   void fetchRandomImage() throws Exception {
     mockMvc.perform(get("/images/random"))
         .andExpect(status().isOk())
-        .andExpect(content().contentType("image/jpe"));
+        .andExpect(content().contentType("image/jpeg"));
   }
 }
